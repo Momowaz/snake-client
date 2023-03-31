9a9dd38ded1802026119acd15bfa85e7bc5c7d49
MohibHome <mohib@can-health.org>
2023-03-31 07:21:14 -0700

    initial client code setup

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = () => {
+    const conn = net.createConnection({
+    host: 'localhost', // change to IP address of computer, more on that below
+    port: 50541,
+});
+
+// Recieve data from server
+conn.on('data', (data) => {
+    console.log(data);
+})
+
+conn.setEncoding('utf8');
+return conn;
+}
+
+console.log('Connecting...');
+connect();
+
